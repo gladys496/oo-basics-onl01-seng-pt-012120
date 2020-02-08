e4c6aa6c652d5e7b2439e887a8e0529c32b25d23
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-02-08 22:03:38 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,8 +2,8 @@ class Shoe
   attr_accessor :brand, :color, :size, :material, :condition
   attr_reader :brand
  
-  def initialize(title)
-    @title = title
+  def initialize(brand)
+    @brand = brand
   end
  
   def repair_shoe

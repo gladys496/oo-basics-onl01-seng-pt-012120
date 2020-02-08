246145c72bca09ca295e616b891e25424ae40979
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-02-08 22:21:27 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,11 +5,8 @@ class Shoe
   def initialize(brand)
     @brand = brand
   end
- 
-  def cobble
-    puts "Your shoe is as good as new!"
-  end
-  def cobble
+ def cobble
     self.condition = "new"
   end 
+  
 end
